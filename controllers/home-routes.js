50a98a775158d114ec7804f363db71b19aa9b720
spurgason <shanepurgason.98@gmail.com>
2021-10-27 17:03:31 -0400

    pushing for review

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,36 @@
 const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-  res.render('homepage');
+  console.log('======================');
+  Post.findAll({
+      attributes: [
+          'id',
+          'post_url',
+          'title',
+          'created_at'
+      ],
+      include: [
+          {
+              model: Comment,
+              attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+              include: {
+                  model: User,
+                  attributes: ['username']
+              }
+          },
+          {
+              model: User,
+              attributes: ['username']
+          }
+      ]
+  })
+      .then(postData => {
+        console.log(postData[0]);
+      })
+      .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+      });
 });
-
 module.exports = router;
