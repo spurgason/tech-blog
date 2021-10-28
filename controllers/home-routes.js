816faa01c944427b053338b8178e32c84e0adffe
spurgason <shanepurgason.98@gmail.com>
2021-10-28 13:16:24 -0400

    push for review

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,36 +2,41 @@ const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-  console.log('======================');
-  Post.findAll({
-      attributes: [
-          'id',
-          'post_url',
-          'title',
-          'created_at'
-      ],
-      include: [
-          {
-              model: Comment,
-              attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-              include: {
-                  model: User,
-                  attributes: ['username']
-              }
-          },
-          {
-              model: User,
-              attributes: ['username']
-          }
-      ]
-  })
-      .then(postData => {
-        const posts = postData.map(post => post.get({ plain: true }));
-        res.render('homepage', {posts});
-      })
-      .catch(err => {
-          console.log(err);
-          res.status(500).json(err);
-      });
+    console.log('======================');
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(postData => {
+            const posts = postData.map(post => post.get({ plain: true }));
+            res.render('homepage', { posts });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
 module.exports = router;
