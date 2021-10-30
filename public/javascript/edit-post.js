90b211937464c4fd769089e4449464dfe71d7714
spurgason <shanepurgason.98@gmail.com>
2021-10-29 23:25:51 -0400

    pushing for review

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -2,7 +2,7 @@ async function editFormHandler(event) {
     event.preventDefault();
 
     const title = document.querySelector('input[name="post-title"]').value.trim();
-    const body = document.querySelector('input[name="post-body"]').value.trim();
+    const body = document.querySelector('textarea[name="post-body"]').value.trim();
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
